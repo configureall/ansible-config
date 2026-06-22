d7ca6fbec679a7cfb71dd36016245e145c37e55f
configureall <akashpandiyan5@gmail.com>
2026-06-23 01:00:26 +0530

    Refactor Ansible and Jenkins installation script
    Updated Jenkins installation process and ensured Ansible is installed correctly.

--- ansibleandjenkins.sh
+++ ansibleandjenkins.sh
@@ -2,35 +2,27 @@
 
 set -e
 
-echo "Updating packages..."
 sudo apt update -y
-
-echo "Installing prerequisites..."
-sudo apt install -y software-properties-common wget curl gnupg
-
-echo "Installing Ansible..."
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
-
-echo "Installing Java..."
 sudo apt install -y fontconfig openjdk-21-jre
-
-echo "Installing Jenkins..."
-sudo mkdir -p /etc/apt/keyrings
+java -version
 
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/" | \
-sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install -y jenkins
 
 echo "Starting Jenkins..."
 sudo systemctl enable jenkins
 sudo systemctl restart jenkins
+
+sudo apt update -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+
 
 echo ""
 echo "===== Versions ====="
